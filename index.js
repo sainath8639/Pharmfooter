8e5182710f3e0ffe3645c342dd66247736770463
Sainath Reddy <sainatha.bontha@sprinklr.com>
2023-05-23 10:12:50 +0530

    added

--- index.js
+++ index.js
@@ -4,22 +4,43 @@ let aboutUs = document.getElementById("aboutUs");
 
 aboutUs.onclick = () => {
   let elem = document.getElementsByClassName("footer-about_us-links");
-  if (elem[0].classList.contains("none")) elem[0].classList.remove("none");
-  else elem[0].classList.add("none");
+  let arrow = document.getElementsByClassName("arrow-icon")[0];
+
+  if (elem[0].classList.contains("none")) {
+    elem[0].classList.remove("none");
+    arrow.src = "./images/downArrow.png";
+  } else {
+    elem[0].classList.add("none");
+    arrow.src = "./images/right-arrow.png";
+  }
 };
 
 let ourServices = document.getElementById("ourServices");
 
 ourServices.onclick = () => {
   let elem = document.getElementsByClassName("footer-services-links");
-  if (elem[0].classList.contains("none")) elem[0].classList.remove("none");
-  else elem[0].classList.add("none");
+  let arrow = document.getElementsByClassName("arrow-icon")[1];
+
+  if (elem[0].classList.contains("none")) {
+    elem[0].classList.remove("none");
+    arrow.src = "./images/downArrow.png";
+  } else {
+    elem[0].classList.add("none");
+    arrow.src = "./images/right-arrow.png";
+  }
 };
 
 let partners = document.getElementById("partners");
 
 partners.onclick = () => {
   let elem = document.getElementsByClassName("footer-partners-links");
-  if (elem[0].classList.contains("none")) elem[0].classList.remove("none");
-  else elem[0].classList.add("none");
+  let arrow = document.getElementsByClassName("arrow-icon")[2];
+
+  if (elem[0].classList.contains("none")) {
+    elem[0].classList.remove("none");
+    arrow.src = "./images/downArrow.png";
+  } else {
+    elem[0].classList.add("none");
+    arrow.src = "./images/right-arrow.png";
+  }
 };
